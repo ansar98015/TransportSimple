711cd120ee8e51df465257545ea7d3461587d408
Ansar Khan <ansarkhan98015@gmail.com>
2025-04-28 11:57:16 +0530

    filter func by zilmoney

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,50 +2,51 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, FormsModule],
+  imports: [RouterOutlet, CommonModule, FormsModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
 
-  trips: { start: string, end: string, id: number }[] = [];
-  newTrip = { start: '', end: '' };
-  nextId = 1;
+  userData:any[] = [];
+  searchData:any[] = [];
 
-  addTrip() {
-    if (this.newTrip.start && this.newTrip.end) {
-      this.trips.push({
-        start: this.newTrip.start.toUpperCase().substring(0, 3),
-        end: this.newTrip.end.toUpperCase().substring(0, 3),
-        id: this.nextId++
-      });
-      this.newTrip = { start: '', end: '' };
+  constructor(private http:HttpClient) {}
+
+  ngOnInit() {
+    this.getDataFromHttpReq();
+  }
+
+  getDataFromHttpReq() {
+    this.http.get<any[]>('https://zil-test.s3.us-east-1.amazonaws.com/json-data.json')
+    .pipe(catchError((error) => throwError((error:any) =>error )))
+    .subscribe((data) => {
+      this.userData = data;
+      this.searchData = data;
+    })
+  }
+
+  filterByValue(e:any, type:string){
+    let searchValue = e.target.value.toLowerCase();
+
+    if (type=='name') {
+      this.searchData = this.userData.filter((item) => {
+        return item.name.toLowerCase().includes(searchValue);
+      })
+    }
+    else if(type=='comp'){
+      this.searchData = this.userData.filter((item) => (item.company.name).toLowerCase().includes(searchValue));
+    }
+    else if(type=='desi'){
+      this.searchData = this.userData.filter((item) => (item.company.designation).toLowerCase().includes(searchValue))
+    }else {
+      this.searchData = this.userData;
     }
   }
-
-  isContinued(index: number): boolean {
-    if (index === 0) return false;
-    return this.trips[index - 1].end === this.trips[index].start;
-  }
-
-  isDuplicate(index: number): boolean {
-    if (index === 0) return false;
-    return this.trips[index - 1].start === this.trips[index].start &&
-      this.trips[index - 1].end === this.trips[index].end;
-  }
-
-  getTripLevel(index: number): number {
-    if (this.isDuplicate(index)) return 2;
-    if (this.isContinued(index)) return 1;
-    return 0;
-  }
-
-  removeTrip(id: number) {
-    this.trips = this.trips.filter(trip => trip.id !== id);
-  }
-
 }
